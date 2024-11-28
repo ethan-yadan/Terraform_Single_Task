4b00cd61dcfc81a9eef6e4bdbffca038f4fe19ae
Ethan Yadan <152818660+ethan-yadan@users.noreply.github.com>
2024-11-28 17:37:26 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,18 @@
+function docker_install(){
+
+    sudo apt update -y && sudo apt install -y docker-compose 
+}
+
+
 function terraform_install(){
 
     wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+    # fix faye ==> ''' sudo vi /etc/apt/sources.list.d/hashicorp.list ''' (change faye to bookworm, if you work with linux mint) ( ubuntu, change to jammy)
     sudo apt update && sudo apt install terraform   
 }
 
-function docker_install(){
-    sudo apt update -y && sudo apt install -y docker-compose 
-}
+
 
 function docker_build(){
 $ docker run --name docker-nginx -p 80:80 -v ./content:/usr/share/nginx/html:ro -d nginx
